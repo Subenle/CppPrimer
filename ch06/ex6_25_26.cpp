d28f12d6ff8a06049ac1af6a50e5f34c8e14fdd1
Subly <benle126@126.com>
2016-08-23 13:25:09 +0800

    Change variable i in the loop begin from 0
    When argc is equal to 1, argv[0] should be printed.

--- ch06/ex6_25_26.cpp
+++ ch06/ex6_25_26.cpp
@@ -13,7 +13,7 @@
 int main(int argc, char** argv)
 {
     std::string str;
-    for (int i = 1; i != argc; ++i) {
+    for (int i = 0; i < argc; ++i) {
         str += argv[i];
         str += " ";
     }
